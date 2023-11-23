77bdccdf4180df8c1ee8a4e0cddad9adf97fa34f
sammich-dev <sammich-game@proton.me>
2023-11-23 14:00:51 +0100

    /api/played-games/:from

--- server/src/app.config.ts
+++ server/src/app.config.ts
@@ -8,6 +8,9 @@ import { matchMaker } from "colyseus"
  */
 import {GameRoom} from "./rooms/GameRoom";
 import {createRoom} from "@colyseus/core/build/MatchMaker";
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient();
+
 
 export default config({
 
@@ -21,26 +24,37 @@ export default config({
     },
 
     initializeExpress: (app) => {
-        /**
-         * Bind your custom express routes here:
-         * Read more: https://expressjs.com/en/starter/basic-routing.html
-         */
-        app.get("/hello_world", (req, res) => {
-            res.send("It's time to kick ass and chew bubblegum!");
+        app.get("/api/played-games/:from", async (req, res) => {
+            const rows = await prisma.playedMatch.findMany({
+                take:100,
+                where:{
+                    ID:{gte:Number(req.params.from)}
+                }
+            });
+            const lastPlayedGameId = (await prisma.playedMatch.findFirst({orderBy:{ID:"desc"}})).ID;
+            return res.send({
+                results:rows,
+                lastPlayedGameId
+            });
+        });
+
+        app.get("/api/last-played-game-id", async (req, res) => {
+            const lastPlayedGameId = (await prisma.playedMatch.findFirst({orderBy:{ID:"desc"}})).ID;
+            return res.send(lastPlayedGameId)
         });
 
         /**
          * Use @colyseus/playground
          * (It is not recommended to expose this route in a production environment)
-         */
+
         if (process.env.NODE_ENV !== "production") {
             app.use("/", playground);
-        }
+        }         */
 
         /**
          * Use @colyseus/monitor
          * It is recommended to protect this route with a password
-         * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
+         * //TODO Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
          */
         app.use("/monitor", monitor());
     },
